15f94c63e4e17be5406c3b78b88540f323c78847
Zinath22 <Zinath22@users.noreply.github.com>
2023-12-06 22:15:20 +0600

    side bar add

--- src/components/ui/SideBar/SideBar.js
+++ src/components/ui/SideBar/SideBar.js
@@ -1,75 +1,33 @@
-import {
-    Box,
-    Card,
-    CardActionArea,
-    CardContent,
-    CardMedia,
-    Typography,
-    Stack,
-    Divider,
-  } from "@mui/material";
-  import Image from "next/image";
-  import sideBarNews from "../../../assets/Rectangle 8.png";
-  import sideBottomNews from "../../../assets/Rectangle 13.png";
-  import SidebarNewsCard from "./SideBarNewsCard";
-  
-  const SideBar = () => {
-    return (
-      <Box className="my-5">
-        <Card>
-          <CardActionArea>
-            <CardMedia>
-              <Image src={sideBarNews} width={800} alt="top news" />
-            </CardMedia>
-            <CardContent>
-              <p 
-                className="
-                        w-[100px]
-                        bg-red-500
-                        px-2
-                        text-white
-                        my-5
-                        rounded
-                        "
-              >
-                Technology
-              </p>
-              <Typography gutterBottom variant="h5" component="div">
-                Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
-              </Typography>
-              <Typography gutterBottom className="my-3">
-                By Tanmoy Parvez - Mar 18 2023
-              </Typography>
-  
-              <Typography variant="body2" color="text.secondary">
-                It is a long established fact that a reader will be distracted by
-                the readable content of a page when looking at its layout........
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Divider />
-  
-        <Stack my={3} columnGap={2}>
-          <SidebarNewsCard />
-          <SidebarNewsCard />
-          <SidebarNewsCard />
-          <SidebarNewsCard />
-          <SidebarNewsCard />
-          <SidebarNewsCard />
-          <SidebarNewsCard />
-        </Stack>
-  
-        <Box>
-          <Image
-            src={sideBottomNews}
-            height={400}
-            width={350}
-            alt="rectangle world image"
-          />
-        </Box>
-      </Box>
-    );
-  };
-  
-  export default SideBar;
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import Image from "next/image";
+import sideBarNews from "../../../assets/Rectangle 9 (1).png";
+
+
+const SideBar = () => {
+  return (
+    <Box>
+                <Card className="my-5">
+      <CardActionArea>
+      <CardMedia>
+               <Image src={sideBarNews} alt="topNews" width={800}></Image>
+      </CardMedia>
+       <CardContent>
+        <p className="w-[100px] my-5 px-2 bg-red-500 text-white">Technology</p>
+        <Typography gutterBottom variant="h5" component="div">
+        Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
+        </Typography>
+        <Typography gutterBottom  className="my-3">
+        By Awlad Hossain - Mar 18 2023
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
+        </Typography>
+      </CardContent>
+      
+      </CardActionArea>
+    </Card>
+    </Box>
+  );
+};
+
+export default SideBar;
